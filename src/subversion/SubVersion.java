78b8d60859ac14180af5c3a97a3a6784c7c4c955
Jose <Jose@10.10.44.66>
2016-04-18 16:05:26 +0200

    versió 0.1 del projecte subVersion

---
+++ src/subversion/SubVersion.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package subversion;
+
+import java.util.Arrays;
+
+/**
+ *
+ * @author Jose
+ */
+public class SubVersion {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int[] nums = new int[5];
+
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = (int) (Math.random() * 10);
+
+        }
+        System.out.println("Array" + Arrays.toString(nums));
+        int menor = nums[0];
+        for (int j = 0; j < nums.length; j++) {
+            if (nums[j] < menor) {
+                menor = nums[j];
+            }
+        }
+
+        System.out.println("El numero mes petit és " + menor);
+    }
+}
